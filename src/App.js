f62b584874e0a4b4bb7eb5f35247fcd74807ef3c
Peter-Lavigne <peterklavigne@gmail.com>
2023-02-13 14:37:35 -0500

    Update Shane

--- src/App.js
+++ src/App.js
@@ -1,234 +1,271 @@
 import './App.css';
-import { ComprehendClient, DetectSentimentCommand } from "@aws-sdk/client-comprehend";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const { Configuration, OpenAIApi } = require("openai");
 
-const openingLine = 'Shane has just arrived at the farm after a busy day at work.  He catches sight of you.';
-
-const SENTIMENTS = {
-  HAPPY: 1,
-  PLEASED: 2,
-  NEUTRAL: 3,
-  ANNOYED: 4,
-  CONCERNED: 5
+const PORTRAIT_URLS = {
+  'NEUTRAL': "https://stardewvalleywiki.com/mediawiki/images/8/8b/Shane.png",
+  'HAPPY': "https://stardewvalleywiki.com/mediawiki/images/9/92/Shane_Happy.png",
+  'CONCERNED': "https://stardewvalleywiki.com/mediawiki/images/9/9d/Shane_Concerned.png",
+  'NEUTRAL_DISTRACTED': "https://stardewvalleywiki.com/mediawiki/images/3/31/Shane_Neutral.png",
+  'BLUSHING': "https://stardewvalleywiki.com/mediawiki/images/e/e1/Shane_Blushing.png",
+  'ANNOYED': "https://stardewvalleywiki.com/mediawiki/images/c/c7/Shane_Annoyed.png",
+  'SURPRISED': "https://stardewvalleywiki.com/mediawiki/images/d/d3/Shane_Surprised.png",
+  'PLEASED': "https://stardewvalleywiki.com/mediawiki/images/f/ff/Shane_Pleased.png",
+  'WITH_CHICKEN_HAPPY': "https://stardewvalleywiki.com/mediawiki/images/0/08/Shane_and_Charlie.png",
+  'WITH_CHICKEN_CONCERNED': "https://stardewvalleywiki.com/mediawiki/images/5/55/Shane_and_Charlie_2.png",
+  'FALLEN_DOWN_DRUNK': "https://stardewvalleywiki.com/mediawiki/images/e/eb/Shane_Fallen_Down.png",
+  'BEACH_NEUTRAL': "https://stardewvalleywiki.com/mediawiki/images/f/f1/Shane_Beach.png",
+  'BEACH_HAPPY': "https://stardewvalleywiki.com/mediawiki/images/d/d1/Shane_Beach_Happy.png",
+  'BEACH_CONCERNED': "https://stardewvalleywiki.com/mediawiki/images/2/25/Shane_Beach_Concerned.png",
 }
 
-const SENTIMENT_URLS = {
-  [SENTIMENTS.HAPPY]: 'https://stardewvalleywiki.com/mediawiki/images/9/92/Shane_Happy.png',
-  [SENTIMENTS.PLEASED]: 'https://stardewvalleywiki.com/mediawiki/images/f/ff/Shane_Pleased.png',
-  [SENTIMENTS.NEUTRAL]: 'https://stardewvalleywiki.com/mediawiki/images/8/8b/Shane.png',
-  [SENTIMENTS.ANNOYED]: 'https://stardewvalleywiki.com/mediawiki/images/c/c7/Shane_Annoyed.png',
-  [SENTIMENTS.CONCERNED]: 'https://stardewvalleywiki.com/mediawiki/images/9/9d/Shane_Concerned.png',
+const OPENING_LINE = 'Shane has just arrived at the farm after a busy day at work. He catches sight of you.';
+
+const openingScene = (name) => {
+  return `* ${name} is a farmer who asks the most important questions. Shane is a bartender in Pelican Town who is often rude and unhappy, and suffers from alcohol dependence. However, his attitude has started to improve toward ${name} as they become better friends. Shane enjoys telling stories about himself. Shane has just arrived at the farm after a busy day at work. He has a crazy story to tell ${name}. He catches sight of ${name}. *`;
 }
 
-const configuration = new Configuration({
-  apiKey: '<<<Add OpenAi API key>>>'
-});
-const openai = new OpenAIApi(configuration);
+const MIN_PLAYER_CHOICES = 2;
+const MAX_PLAYER_CHOICES = 3;
 
-const sentimentClient = new ComprehendClient({
-  region: 'us-east-1',
-  credentials: {
-    accessKeyId: '<<<Add AWS key>>>',
-    secretAccessKey: '<<<Add AWS secret KEY>>>'
-  }
-});
-
-const getNextDialogue = async (prompt, suffix, n, max_tokens, model) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: prompt,
-    temperature: 0.9,
-    max_tokens: max_tokens,
-    n: n,
-    stop: [suffix, '\n'],
-    suffix: suffix
-  });
-  return response;
+const SAY_NOTHING_OPTION = "* Say nothing. *";
+const sayNothingDialogue = (name) => {
+  return `* ${name} says nothing. *`;
 }
 
-const analyzeSentiment = async (text) => {
-  const command = new DetectSentimentCommand({
-    LanguageCode: 'en',
-    Text: text
-  });
-  const response = await sentimentClient.send(command);
-  return response;
-}
-
-// `story` should end with `Shane: ` without a newline
-const getShaneDialogue = async (story, name) => {
-  const response = await getNextDialogue(
-    story,
-    `\n${name}: `,
-    1,
-    100
-  )
-  let rawText = response.data.choices[0].text;
-  while (rawText.length > 0 && rawText[0] === '\n') {
-    rawText = rawText.substring(1);
-  }
-  return rawText.split(/\n/)[0];
-}
-
-// `story` should end with `${name}: ` without a newline
-const getPlayerOptions = async (story) => {
-  const optionsResponse = await getNextDialogue(
-    story,
-    `\nShane: `,
-    2,
-    20
-  )
-  return optionsResponse.data.choices.map(choice => choice.text.split(/\n/)[0]);
-}
-
-const say = async (name, story, phrase, setStory, setLastDialog, setOptions, setCustomOption, loading, setLoading, setSentiment, setStarted) => {
-  if (loading) {
-    return;
-  }
-  const sayNothing = '* say nothing *';
-  if (phrase === sayNothing) {
-    phrase = `* ${name} says nothing *`
-  }
-  setLoading(true);
-  story = `${story}${phrase}\nShane: `;
-  let dialogue = await getShaneDialogue(story, name);
-  if (dialogue == '') {
-    dialogue = '* Shane says nothing *';
-  }
-  const sentiment = await analyzeSentiment(dialogue);
-  setLastDialog(dialogue);
-  setSentiment(sentiment);
-  setStarted(true);
-  story = `${story}${dialogue}\n${name}: `
-  let options = await getPlayerOptions(story);
-  options = options.map(option => {
-    if (option === '') {
-      return sayNothing;
-    }
-    return option;
-  })
-  setStory(story);
-  setOptions([...new Set(options)]);
-  setCustomOption('');
-  setLoading(false);
-}
-
-const start = async (name, setStory, setLastDialog, setOptions, setStarted, loading, setLoading, setSentiment) => {
-  if (loading) {
-    return;
-  }
-  const context = `* ${name} is a farmer who asks a lot of philosophical questions. Shane is a bartender in Pelican Town who is often rude and unhappy, and suffers from alcohol dependence. However, his attitude has started to improve toward ${name} as they become better friends. Shane enjoys telling stories about himself. Shane has just arrived at the farm after a busy day at work. He has a crazy story to tell ${name}. He catches sight of ${name}. *`;
-  await say(name, `${context}\n`, '', setStory, setLastDialog, setOptions, () => { }, loading, setLoading, setSentiment, setStarted);
-}
-
-const getPortrait = (sentiment) => {
-  if (sentiment === null) {
-    return SENTIMENT_URLS[SENTIMENTS.NEUTRAL]
-  }
-
-  const positiveScore = sentiment.SentimentScore['Positive'];
-  const negativeScore = sentiment.SentimentScore['Negative'];
-
-  if (positiveScore < 0.2 && negativeScore < 0.2) {
-    return SENTIMENT_URLS[SENTIMENTS.NEUTRAL];
-  }
-
-  if (positiveScore > 0.8 && negativeScore < 0.5) {
-    return SENTIMENT_URLS[SENTIMENTS.HAPPY];
-  }
-  if (negativeScore > 0.8 && positiveScore < 0.5) {
-    return SENTIMENT_URLS[SENTIMENTS.CONCERNED];
-  }
-
-  if (positiveScore - negativeScore > 0.1) {
-    return SENTIMENT_URLS[SENTIMENTS.PLEASED];
-  }
-  if (negativeScore - positiveScore > 0.1) {
-    return SENTIMENT_URLS[SENTIMENTS.PLEASED];
-  }
-
-  return SENTIMENT_URLS[SENTIMENTS.NEUTRAL];
-}
+let story = null;
 
 function App() {
-  const [story, setStory] = useState(null);
-  const [lastDialog, setLastDialog] = useState(openingLine);
-  const [started, setStarted] = useState(false);
-  const [options, setOptions] = useState([]);
+  const [mostRecentShaneDialogue, setMostRecentShaneDialogue] = useState(null);
+  // [string, string] containing the player's option and Shane's response
+  const [optionsWithResponses, setOptionsWithResponses] = useState(null);
   const [customOption, setCustomOption] = useState('');
   const [loading, setLoading] = useState(false);
-  const [sentiment, setSentiment] = useState(null);
+  const [sentimentUrl, setSentimentUrl] = useState(PORTRAIT_URLS['NEUTRAL']);
   const [name, setName] = useState('');
+  const [openAIClient, setOpenAIClient] = useState(null);
+
+  const onClickReset = () => {
+    story = null;
+    setMostRecentShaneDialogue(null);
+    setOptionsWithResponses(null);
+    setCustomOption('');
+    setLoading(false);
+    setSentimentUrl(PORTRAIT_URLS['NEUTRAL'])
+  }
+
+  const createCompletion = async (
+    prompt,
+    num_responses,
+    max_generated_tokens,
+    stop = null,
+  ) => {
+    const response = await openAIClient.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.9,
+      max_tokens: max_generated_tokens,
+      n: num_responses,
+      stop: stop ? [stop] : undefined,
+      suffix: stop
+    });
+    return response;
+  }
+
+  const detectSentimentUrl = async (text) => {
+    const options = Object.keys(PORTRAIT_URLS).join(', ');
+    const prompt = `Classify the following phrase as one of the following: ${options}\n${text}\n`;
+    const response = await createCompletion(prompt, 1, 15);
+    const responseText = response.data.choices[0].text.trim();
+    if (Object.keys(PORTRAIT_URLS).includes(responseText)) {
+      return PORTRAIT_URLS[responseText];
+    } else {
+      return PORTRAIT_URLS['NEUTRAL'];
+    }
+  }
+
+  // `story` should end with `Shane: ` without a newline
+  const getShaneDialogue = async () => {
+    const response = await createCompletion(
+      story,
+      1,
+      100,
+      `\n${name}: `,
+    )
+    let rawText = response.data.choices[0].text.trim();
+    const processedText = rawText.split(/\n/)[0];
+    if (processedText === '') {
+      return sayNothingDialogue(name);
+    } else {
+      return processedText;
+    }
+  }
+
+  // `story` should end with `${name}: ` without a newline
+  const getPlayerOptionsWithShaneResponses = async () => {
+    const optionsResponse = await createCompletion(
+      story,
+      MAX_PLAYER_CHOICES,
+      120,
+      `\n${name}: `,
+    )
+    const shaneLineStart = `Shane: `;
+    const choices = optionsResponse.data.choices
+      .map(choice => choice.text.split(/\n/).filter(line => line !== ''))
+      .filter(lines => lines.length > 0)
+      .map(lines => {
+        if (lines.length > 1 && lines[1].startsWith(shaneLineStart)) {
+          return [
+            lines[0],
+            lines[1].substring(shaneLineStart.length)
+          ];
+        } else {
+          return [lines[0], null];
+        }
+      })
+    const uniqueChoices = choices.filter((choice, index) => {
+      return choices.findIndex((otherChoice) => {
+        return otherChoice[0] === choice[0];
+      }) === index;
+    });
+    if (uniqueChoices.length < MIN_PLAYER_CHOICES) {
+      uniqueChoices.push([SAY_NOTHING_OPTION, null])
+    }
+    return uniqueChoices;
+  }
+
+  const immediatelyProcessShaneDialogue = async (dialogue) => {
+    const sentimentUrl = await detectSentimentUrl(dialogue);
+    setMostRecentShaneDialogue(dialogue);
+    setSentimentUrl(sentimentUrl);
+  }
+
+  const processPlayerDialogue = async (dialogue, shane_response = null) => {
+    if (loading) return;
+    setLoading(true);
+    if (dialogue === SAY_NOTHING_OPTION) {
+      dialogue = sayNothingDialogue(name);
+    }
+    story = `${story}${dialogue}\nShane: `;
+    if (shane_response === null) {
+      shane_response = await getShaneDialogue();
+    }
+    story = `${story}${shane_response}\n${name}: `;
+    const [_, options] = await Promise.all([
+      immediatelyProcessShaneDialogue(shane_response),
+      getPlayerOptionsWithShaneResponses()
+    ]);
+    setOptionsWithResponses(options);
+    setCustomOption('');
+    setLoading(false);
+  }
+
+  const onStart = async () => {
+    if (loading) return;
+    setLoading(true);
+    story = `${openingScene(name)}\n\nShane: `;
+    const shane_response = await getShaneDialogue();
+    story = `${story}${shane_response}\n${name}: `;
+    const [sentimentUrl, options] = await Promise.all([
+      detectSentimentUrl(shane_response),
+      getPlayerOptionsWithShaneResponses(),
+    ]);
+    setMostRecentShaneDialogue(shane_response);
+    setSentimentUrl(sentimentUrl);
+    setOptionsWithResponses(options);
+    setLoading(false);
+  }
+
+  const onSubmitPredefinedOption = (option) => {
+    processPlayerDialogue(option[0], option[1]);
+  }
+
+  const onSubmitCustomOption = () => {
+    processPlayerDialogue(customOption);
+  }
+
+  const createOpenAIApiClient = (apiKey) => {
+    const configuration = new Configuration({
+      apiKey: apiKey
+    });
+    setOpenAIClient(new OpenAIApi(configuration));
+  }
+
+  useEffect(() => {
+    if (process.env.REACT_APP_OPENAI_KEY !== undefined) {
+      createOpenAIApiClient(process.env.REACT_APP_OPENAI_KEY);
+    }
+  }, []);
 
   return (
     <div className="App">
       <div className="flex-container">
-        <img className='portrait' src={getPortrait(sentiment)} />
+        <img className='portrait' src={sentimentUrl} />
         <div className='dialogue-box-outer'>
           <div className='dialogue-box-inner'>
             {
-              started ? (
-                <p className='dialogue shane-dialogue'>{lastDialog}</p>
-              ) : (
-                <i className='dialogue'>{lastDialog}</i>
-              )
-            }
-            <hr />
-            {
-              started ? (
+              openAIClient ? (
                 <>
                   {
-                    options.map(option =>
-                      <button
-                        key={option}
-                        className='dialogue dialogue-option'
-                        disabled={loading}
-                        onClick={() => say(name, story, option, setStory, setLastDialog, setOptions, setCustomOption, loading, setLoading, setSentiment, setStarted)}
-                      >
-                        {option}
-                      </button>
+                    mostRecentShaneDialogue === null ? (
+                      <i className='dialogue'>{OPENING_LINE}</i>
+                    ) : (
+                      <p className='dialogue shane-dialogue'>{mostRecentShaneDialogue}</p>
                     )
                   }
-                  <form className='dialog dialog-option' onSubmit={(e) => {
-                    e.preventDefault();
-                    say(name, story, customOption, setStory, setLastDialog, setOptions, setCustomOption, loading, setLoading, setSentiment, setStarted);
-                  }}>
-                    <input disabled={loading} placeholder={'Custom option'} className='dialog dialog-option custom-input' type="text" value={customOption} onInput={(e) => setCustomOption(e.target.value)} />
-                  </form>
                   <hr />
-                  <button
-                    className='dialogue dialogue-option reset'
-                    disabled={loading}
-                    onClick={() => {
-                      setStory(null);
-                      setLastDialog(openingLine);
-                      setStarted(false);
-                      setOptions([]);
-                      setCustomOption('');
-                      setLoading(false);
-                      setSentiment(null);
-                    }}
-                  >
-                    Reset game
-                  </button>
+                  {
+                    (optionsWithResponses === null) ? (
+                      <>
+                        <form className='dialog dialog-option' onSubmit={(e) => {
+                          e.preventDefault();
+                          onStart();
+                        }}>
+                          <label htmlFor="name">Your name</label>
+                          <input id="name" disabled={loading} placeholder={'What is your name?'} className='dialog dialog-option custom-input' type="text" value={name} onInput={(e) => setName(e.target.value)} />
+                        </form>
+                        <button
+                          className='dialogue dialogue-option'
+                          onClick={onStart}
+                          disabled={loading || name === ''}
+                        >
+                          <i>Continue</i>
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                      {
+                        optionsWithResponses.map(option =>
+                          <button
+                            key={option[0]}
+                            className='dialogue dialogue-option'
+                            disabled={loading}
+                            onClick={() => onSubmitPredefinedOption(option)}
+                          >
+                            {option[0]}
+                          </button>
+                        )
+                      }
+                      <form className='dialog dialog-option' onSubmit={(e) => {
+                        e.preventDefault();
+                        onSubmitCustomOption();
+                      }}>
+                        <input disabled={loading} placeholder={'Custom option'} className='dialog dialog-option custom-input' type="text" value={customOption} onInput={(e) => setCustomOption(e.target.value)} />
+                      </form>
+                      <hr />
+                      <button
+                        className='dialogue dialogue-option reset'
+                        disabled={loading}
+                        onClick={onClickReset}
+                      >
+                        Reset game
+                      </button>
+                    </>
+                    )
+                  }
                 </>
               ) : (
-                <>
-                  <form className='dialog dialog-option' onSubmit={(e) => {
-                    e.preventDefault();
-                    start(name, setStory, setLastDialog, setOptions, setStarted, loading, setLoading, setSentiment);
-                  }}>
-                    <input disabled={loading} placeholder={'What is your name?'} className='dialog dialog-option custom-input' type="text" value={name} onInput={(e) => setName(e.target.value)} />
-                  </form>
-                  <button
-                    className='dialogue dialogue-option'
-                    onClick={() => start(name, setStory, setLastDialog, setOptions, setStarted, loading, setLoading, setSentiment)}
-                    disabled={loading || name === ''}
-                  >
-                    <i>Continue</i>
-                  </button>
-                </>
+                <p>Please set the environment variable REACT_APP_OPENAI_KEY</p>
               )
             }
           </div>
